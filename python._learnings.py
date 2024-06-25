f3656541b4f3083a1b262f7d62910ef17d8e63c1
NinjaRider019F <jjaatinsharma123@gmail.com>
2024-06-25 12:34:36 +0530

    Create python._learnings.py

---
+++ python._learnings.py
@@ -0,0 +1,7 @@
+a = 10
+b = 2
+
+print("The value of", a, "+", 3, "is: ", a + b)
+print("The value of", a, "-", 3, "is: ", a - b)
+print("The value of", a, "*", 3, "is: ", a * b)
+print("The value of", a, "/", 3, "is: ", a / b) 
